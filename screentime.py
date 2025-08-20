356a0d35c6c689b5431b48e5a8bbb3215f98012a
Luka Rantalainen <luka.j.rantalainen@gmail.com>
2025-08-20 20:49:46 +0300

    counts time.

--- screentime.py
+++ screentime.py
@@ -3,34 +3,37 @@ import win32gui
 import win32process
 import time
 
-time_data = dict()
+time_data = {}
 
-for cycle in range(5):
-    window_id = win32gui.GetForegroundWindow()
 
-    _, pid = win32process.GetWindowThreadProcessId(window_id)
+def get_foreground_exe():
+    hwnd = win32gui.GetForegroundWindow()
+    _, pid = win32process.GetWindowThreadProcessId(hwnd)
+    return psutil.Process(pid).exe()
 
-    path = psutil.Process(pid).exe()
 
-    print(path)
 
-    start_time = 0
 
-    window_title = ""
-    new_window_title = win32gui.GetForegroundWindow()
-
+def track_window_time():
+    current_exe = get_foreground_exe()
     start_time = time.time()
 
+    while True:
+        time.sleep(0.5)
+        new_exe = get_foreground_exe()
+        elapsed_time = time.time() - start_time
 
+        if new_exe != current_exe:
+            
 
+            if current_exe in time_data: 
+                time_data[current_exe] += elapsed_time
+            else:
+                time_data[current_exe] = elapsed_time
 
-    def in_list_check():
-        if str(path) in time_data:
-            time_data[str(path)] += start_time
             print(time_data)
-        else:
-            time_data[str(path)] = start_time
-            print(time_data)
 
-    in_list_check()
-    time.sleep(1)
+        current_exe = new_exe
+        start_time = time.time()
+
+track_window_time()
