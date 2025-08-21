b59246a884c956f4d390e9be7f4d31cf6765decd
Luka Rantalainen <luka.j.rantalainen@gmail.com>
2025-08-21 18:20:32 +0300

    added daily reset for usage time

--- screentime.py
+++ screentime.py
@@ -2,27 +2,45 @@ import psutil
 import win32gui
 import win32process
 import time
+import datetime
+from datetime import datetime, timedelta
 import atexit
+import pickle
+import os
 
 time_data = {}
 
+file_path = os.path.realpath(__file__)
+
 
 def get_foreground_exe():   #Get current exe
-    hwnd = win32gui.GetForegroundWindow()
-    _, pid = win32process.GetWindowThreadProcessId(hwnd)
-    return psutil.Process(pid).exe()
+    try:
+        hwnd = win32gui.GetForegroundWindow()
+        _, pid = win32process.GetWindowThreadProcessId(hwnd)
+        return psutil.Process(pid).exe()
+    except Exception:
+         return None
+
+def save_time_data():
+            with open("time_data.pickle", "wb") as f:
+                pickle.dump(time_data, f, pickle.HIGHEST_PROTOCOL)
+            print("Time data saved at"+file_path)
 
 
+def mainloop():
 
-
-def track_window_time():
     current_exe = get_foreground_exe()  #current_exe = state at the end of the loop
     start_time = time.time()
+    tomorrow_date = datetime.today() + timedelta(days=1)
+        
+
+
 
     while True:
-        time.sleep(0.5)
-        new_exe = get_foreground_exe() 
+        time.sleep(1)
 
+
+        new_exe = get_foreground_exe() 
         if new_exe != current_exe:
             elapsed_time = time.time() - start_time
 
@@ -31,12 +49,20 @@ def track_window_time():
             else:
                 time_data[current_exe] = elapsed_time
 
-            
             current_exe = new_exe
             start_time = time.time()
 
 
+        if datetime.today() >= tomorrow_date:
+            save_time_data()    
+            time_data.clear()
+            tomorrow_date = datetime.today() + timedelta(days=1)
+
+
+
 def exit_handler():
-    print(time_data)
-        
-track_window_time()
+    print("Time data saved at"+file_path)
+    save_time_data()
+atexit.register(exit_handler)
+
+mainloop()
