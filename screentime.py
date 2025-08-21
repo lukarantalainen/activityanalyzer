bb9d3b3f1f0cde1163f87d2d26c2382626395b78
Luka Rantalainen <luka.j.rantalainen@gmail.com>
2025-08-21 06:56:34 +0300

    fixed timer

--- screentime.py
+++ screentime.py
@@ -2,11 +2,12 @@ import psutil
 import win32gui
 import win32process
 import time
+import atexit
 
 time_data = {}
 
 
-def get_foreground_exe():
+def get_foreground_exe():   #Get current exe
     hwnd = win32gui.GetForegroundWindow()
     _, pid = win32process.GetWindowThreadProcessId(hwnd)
     return psutil.Process(pid).exe()
@@ -15,25 +16,27 @@ def get_foreground_exe():
 
 
 def track_window_time():
-    current_exe = get_foreground_exe()
+    current_exe = get_foreground_exe()  #current_exe = state at the end of the loop
     start_time = time.time()
 
     while True:
         time.sleep(0.5)
-        new_exe = get_foreground_exe()
-        elapsed_time = time.time() - start_time
+        new_exe = get_foreground_exe() 
 
         if new_exe != current_exe:
-            
+            elapsed_time = time.time() - start_time
 
             if current_exe in time_data: 
                 time_data[current_exe] += elapsed_time
             else:
                 time_data[current_exe] = elapsed_time
 
-            print(time_data)
+            
+            current_exe = new_exe
+            start_time = time.time()
 
-        current_exe = new_exe
-        start_time = time.time()
 
+def exit_handler():
+    print(time_data)
+        
 track_window_time()
