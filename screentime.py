1e748df8959f39c4e2fc2458b4a3ce69ea7a81e1
Luka Rantalainen <luka.j.rantalainen@gmail.com>
2025-08-24 09:30:12 +0300

    added time interval to usage detection and fixed saving at the end of each day

--- screentime.py
+++ screentime.py
@@ -3,7 +3,7 @@ import win32gui
 import win32process
 import time
 import datetime
-from datetime import datetime, timedelta
+from datetime import date, timedelta
 import atexit
 import json
 import os
@@ -23,41 +23,56 @@ def save_time_data():
     try:
             with open("time_data.json", "w") as f:
                 json.dump(time_data, f)
-                print("Time data saved")
     except IOError:
         print("An IOError has occured.")
 
 
 def mainloop():
 
-    current_exe = get_foreground_exe()  #current_exe = state at the end of the loop
+    current_exe = get_foreground_exe() #state at the end of the loop
     start_time = time.time()
-    tomorrow_date = datetime.today() + timedelta(days=1)
-        
-
-
+    tomorrow_date = datetime.date.today() + timedelta(1)
+    update_interval = 60
+    last_update = 0 
 
     while True:
-        time.sleep(5)
+        time.sleep(5) 
+        new_exe = get_foreground_exe()
+        now = time.time()
 
 
-        new_exe = get_foreground_exe() 
-        if new_exe != current_exe:
-            elapsed_time = time.time() - start_time
-
+        #focus change
+        if new_exe != current_exe: #if focus changes during 5 second sleep > add time and get current exe&time
+            elapsed_time = now - start_time
             if current_exe in time_data: 
                 time_data[current_exe] += elapsed_time
             else:
                 time_data[current_exe] = elapsed_time
-
             current_exe = new_exe
-            start_time = time.time()
+            start_time = now
+            last_update = now
 
-
+        #every 60 sec update
+        if now - last_update >= update_interval:
+            elapsed_time = now - start_time
+            if current_exe in time_data:
+                time_data[current_exe] += elapsed_time
+            else:
+                time_data[current_exe] = elapsed_time
+            start_time = now
+            last_update = now
+            save_time_data()
+        
         if datetime.today() >= tomorrow_date:
+            elapsed_time = now - start_time
+            if current_exe in time_data:
+                time_data[current_exe] += elapsed_time
+            else:
+                time_data[current_exe] + elapsed_time
             save_time_data()    
             time_data.clear()
-            tomorrow_date = datetime.today() + timedelta(days=1)
+            start_time = now
+            tomorrow_date = datetime.date.today() + timedelta(days=1)
 
 
 
